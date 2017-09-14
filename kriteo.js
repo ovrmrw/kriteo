4c445e22ee664e007778c474a2659b1b59af083f
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2017-09-15 00:47:19 +0900

    fix

--- kriteo.js
+++ kriteo.js
@@ -1,7 +1,7 @@
 var key = "___hoge___";
 var value = localStorage.getItem(key);
 if (value) {
-    var redirect_url = "https://www.google.com" + location.search;
+    var redirect_url = "http://qiita.com" + location.search;
     if (document.referrer) {
         var referrer = "referrer=" + encodeURIComponent(document.referrer);
         redirect_url = redirect_url + (location.search ? '&' : '?') + referrer;
