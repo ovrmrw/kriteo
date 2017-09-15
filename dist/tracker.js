d53b1a514f44bf34efda46a09d0ea50381ad9219
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2017-09-15 12:29:22 +0900

    add dist dir

---
+++ dist/tracker.js
@@ -0,0 +1,14 @@
+var key = "___hoge___";
+var url = "https://ovrmrw.github.io/my-first-react-typescript/";
+var value = localStorage.getItem(key);
+if (value) {
+    var redirect_url = url + location.search;
+    if (document.referrer) {
+        var referrer = "ref=" + encodeURIComponent(document.referrer);
+        redirect_url = redirect_url + (location.search ? '&' : '?') + referrer;
+    }
+    location.href = redirect_url + (redirect_url.indexOf('?') > -1 ? '&' : '?') + 'id=' + value;
+} else {
+    var randomValue = Math.floor(99999999999 * Math.random());
+    localStorage.setItem(key, randomValue);
+}
--- kriteo.js
+++ kriteo.js
@@ -1,14 +1,22 @@
-var key = "___hoge___";
-var url = "https://ovrmrw.github.io/my-first-react-typescript/";
-var value = localStorage.getItem(key);
-if (value) {
-    var redirect_url = url + location.search;
-    if (document.referrer) {
-        var referrer = "referrer=" + encodeURIComponent(document.referrer);
-        redirect_url = redirect_url + (location.search ? '&' : '?') + referrer;
-    }
-    location.href = redirect_url + (redirect_url.indexOf('?') > -1 ? '&' : '?') + 'id=' + value;
-} else {
-    var randomValue = Math.random().toString(36).slice(-8);
-    localStorage.setItem(key, randomValue);    
-}
+// var key = "___hoge___";
+// var url = "https://ovrmrw.github.io/my-first-react-typescript/";
+// var value = localStorage.getItem(key);
+// if (value) {
+//     var redirect_url = url + location.search;
+//     if (document.referrer) {
+//         var referrer = "ref=" + encodeURIComponent(document.referrer);
+//         redirect_url = redirect_url + (location.search ? '&' : '?') + referrer;
+//     }
+//     // location.href = redirect_url + (redirect_url.indexOf('?') > -1 ? '&' : '?') + 'id=' + value;
+
+// } else {
+//     var randomValue = Math.floor(99999999999 * Math.random());
+//     localStorage.setItem(key, randomValue);    
+// }
+
+
+var iframe = document.createElement('iframe');
+iframe.width = 0;
+iframe.height = 0;
+iframe.style.display = 'none';
+iframe.src = ''
