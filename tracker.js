2e87f3ba84f1afc1b2c2782da1e4a97a04928a81
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2017-09-15 20:16:49 +0900

    Updates

--- tracker.js
+++ tracker.js
@@ -1,14 +1,20 @@
-var key = "id";
+var userIdKey = "userId";
+var itemIdKey = "itemId";
 var redirectUrlBase = "https://easy-ng-universal.firebaseapp.com/";
 
-var value = localStorage.getItem(key);
-if (value) {
+var viewItemId = getViewItemId();
+if (viewItemId) {
+    localStorage.setItem(itemIdKey, viewItemId);
+}
+
+var userId = localStorage.getItem(userIdKey);
+if (userId) {
     var redirectUrl = redirectUrlBase + location.search;
     if (document.referrer) {
         var referrer = "ref=" + encodeURIComponent(document.referrer);
         redirectUrl = redirectUrl + (location.search ? '&' : '?') + referrer;
     }
-    var finalRedirectUrl = redirectUrl + (redirectUrl.indexOf('?') > -1 ? '&' : '?') + 'id=' + value;
+    var finalRedirectUrl = redirectUrl + (redirectUrl.indexOf('?') > -1 ? '&' : '?') + 'userId=' + userId;
     console.log('Redirect after 3 seconds');
     setTimeout(() => {
         console.log('Redirect to ' + finalRedirectUrl);
@@ -16,5 +22,17 @@ if (value) {
     }, 3000);
 } else {
     var id = Math.floor(99999999999 * Math.random());
-    localStorage.setItem(key, id);
+    localStorage.setItem(userIdKey, id);
+}
+
+
+function getViewItemId() {
+    var pathname = location.pathname;
+    if (pathname.startsWith('/p1')) {
+        return 'p1';
+    } else if (pathname.startsWith('/p2')) {
+        return 'p2';
+    } else {
+        return '';
+    }
 }
