e0b367534231056ddcdac7c491c89bb085e86482
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2017-09-23 21:28:53 +0900

    add postMessage logic

--- kriteo.js
+++ kriteo.js
@@ -1,8 +1,21 @@
+var id = 'kriteo-frame';
+var externalHost = 'https://ovrmrw.github.io'
+
 var iframe = document.createElement('iframe');
+iframe.id = id;
 iframe.width = 0;
 iframe.height = 0;
 iframe.style.display = 'none';
 iframe.style.visibility = 'hidden';
-iframe.src = 'https://ovrmrw.github.io/kriteo/';
+iframe.src = externalHost + '/kriteo/';
 
 document.body.appendChild(iframe);
+
+window.addEventListener('message', function (event) {
+  console.log('event on host:', event);
+}, false);
+
+setTimeout(function () {
+  var w = document.querySelector('#' + id).contentWindow;
+  w.postMessage(location.href, externalHost);
+});
