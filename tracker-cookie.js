3e206961c412af4237f1534953118ae7b5a84208
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2017-09-25 22:24:18 +0900

    Updates

--- tracker-cookie.js
+++ tracker-cookie.js
@@ -1,1 +1,14 @@
-document.cookie = "x=hoge; expires=Tue, 19 Jan 2038 03:14:07 GMT"
+var key = 'kriteoId';
+
+var str = document.cookie.split(';')
+  .map(str => str.trim())
+  .find(str => str.indexOf('=') > -1 && str.split('=')[0] === key);
+
+if (str) {
+  var value = str.split('=')[1];
+  console.log('3rd party Cookieの読み取りに成功しました。 id:', value);
+} else {
+  var id = '' + Math.floor(99999999999 * Math.random());
+  document.cookie = key + '=' + id + '; expires=Tue, 19 Jan 2038 03:14:07 GMT';
+  console.log('3rd party Cookieを新しくセットしました。 id:', id);
+}
