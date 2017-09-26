986c1ece87d940fa0722bf8dbe7be3815ae55d5d
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2017-09-26 16:31:46 +0900

    add redirect.js

---
+++ dist/redirect.js
@@ -0,0 +1,18 @@
+var search = location.search.replace('?', '');
+
+if (search) {
+  var params = search.split('&');
+  var redirectParam = params.find(p => p.split('=')[0] === 'redirect');
+
+  if (redirectParam) {
+    var url = redirectParam.split('=')[1];
+    var redirectUrl = url.indexOf('%') > -1
+      ? decodeURIComponent(url)
+      : url;
+
+    setTimeout(function () {
+      console.log(redirectUrl + 'にリダイレクトします。');
+      location.replace(redirectUrl);
+    }, 1000);
+  }
+}
